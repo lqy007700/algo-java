9575069bac148496726b8cd806a8242a4f9e9f50
zxnl <lqy007700@163.com>
2024-01-05 13:56:24 +0800

    - 动态规划

--- array/src/main/java/array/地下城游戏174.java
+++ array/src/main/java/array/地下城游戏174.java
@@ -1,17 +1,90 @@
 package array;
 
-import java.util.Arrays;
-
 public class 地下城游戏174 {
+    public static void main(String[] args) {
+        地下城游戏174 a = new 地下城游戏174();
+        int[][] dungeon = new int[][]{{-2, -3, 3}, {-5, -10, 1}, {10, 30, -5}};
+        int res = a.calculateMinimumHP(dungeon);
+        System.out.println(res);
+
+        /**
+         {-2, -3, 3},
+         {-5, -10, 1},
+         {10, 30, -5}
+         */
+
+        /**
+         {6, 4, 1},
+         {5, 10, 4},
+         {0, 0, 5}
+         */
+
+    }
+
     public int calculateMinimumHP(int[][] dungeon) {
-        int n = dungeon.length, m = dungeon[0].length;
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        if (dungeon.length == 0 || dungeon[0].length == 0) {
+            return 0;
         }
 
-        System.out.println(Arrays.deepToString(dp));
-        
+        int r = dungeon.length;
+        int c = dungeon[0].length;
+        int[][] dp = new int[r][c];
 
+        dp[r - 1][c - 1] = Math.max(0, -dungeon[r - 1][c - 1]);
+
+        // 最后一行
+        for (int i = c - 2; i >= 0; i--) {
+            int min = dp[r - 1][i + 1] - dungeon[r - 1][i];
+            dp[r - 1][i] = Math.max(0, min);
+        }
+
+        // 最后一列
+        for (int i = r - 2; i >= 0; i--) {
+            int min = dp[i + 1][c - 1] - dungeon[i][c - 1];
+            dp[i][c - 1] = Math.max(0, min);
+        }
+
+        for (int i = r - 2; i >= 0; --i) {
+            for (int j = c - 2; j >= 0; --j) {
+                // 从右边和下边选择一个最小值，然后减去当前的 dungeon 值
+                int needMin = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+                dp[i][j] = Math.max(0, needMin);
+            }
+        }
+
+        return dp[0][0] + 1;
+    }
+
+    public int calculateMinimumHP1(int[][] dungeon) {
+        int r = dungeon.length, c = dungeon[0].length;
+
+        int[][] dp = new int[r][c];
+
+        /**
+         {-2, -3, 3},
+         {-5, -10, 1},
+         {10, 30, -5}
+         */
+        dp[r - 1][c - 1] = Math.max(0, -dungeon[r - 1][c - 1]);
+
+        // 最后一行
+        for (int i = c - 2; i >= 0; i++) {
+            int min = dp[r - 1][i + 1] - dungeon[r - 1][i];
+            dp[r - 1][i] = Math.max(0, min);
+        }
+
+        // 最后一列
+        for (int i = r - 2; i >= 0; i++) {
+            int min = dp[i + 1][c - 1] - dungeon[i][c - 1];
+            dp[i][c - 1] = Math.max(0, min);
+        }
+
+        for (int i = r - 2; i >= 0; i--) {
+            for (int j = c - 2; j >= 0; j--) {
+                int needMin = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+                dp[i][j] = Math.max(0, needMin);
+            }
+        }
+        return dp[0][0] + 1;
     }
 }
